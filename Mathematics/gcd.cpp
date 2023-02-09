d90d037dedede9cde0f8866b48b113f66fd06397
aaqib <aaqibjavaid605@gmail.com>
2023-02-09 15:46:43 +0530

    added gcd problem

---
+++ Mathematics/gcd.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+// Time complexity --> O(a % b)
+// Auxilary Space --> O(1)
+
+int hcf(int a, int b){
+    if(b == 0) return a;
+    return hcf(b, a % b);
+}
+
+int main() {
+    // Function calculates the hcf ( gcd(a, b) = gcd(a - b, b) )
+    cout << hcf(12, 15) << endl;
+    return 0;
+}
