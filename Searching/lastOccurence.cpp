9f8c480d329819528a28218aea210d0a2100edd3
aaqib <aaqibjavaid605@gmail.com>
2023-02-16 12:29:26 +0530

    add index of last occurence of the element

--- Searching/lastOccurence.cpp
+++ Searching/lastOccurence.cpp
@@ -4,14 +4,14 @@ using namespace std;
 // Time complexity --> O(logN)
 // Auxilary Space --> O(1)
 
-int firstOccurence(int arr[], int start, auto end, int key){
+int lastOccurence(int arr[], int start, auto end, int key){
     int ans = -1;
     while(start <= end){
         int mid = start + (end - start) / 2;
         
         if(arr[mid] == key){
             ans = mid;
-            end = mid - 1;
+            start = mid + 1;
         }
         
         if(key > arr[mid]){
@@ -25,10 +25,10 @@ int firstOccurence(int arr[], int start, auto end, int key){
 }
 
 int main(){
-    // Function returns the index of first occurence of the element in a sorted array
-    int arr[] = {1, 10, 10, 10, 20, 20, 30};
+    // Function returns the last occurence of the element in a sorted array
+    int arr[] = {1, 10, 10, 10, 20, 20, 20, 30};
     auto n = sizeof(arr) / sizeof(int);
     int key = 20;
-    cout << firstOccurence(arr, 0, n - 1, key) << endl;
+    cout << lastOccurence(arr, 0, n - 1, key) << endl;
     return 0;
 }
