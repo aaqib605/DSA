798a44d4a73aa3cf993ff7f508d41565c0394dd9
aaqib <aaqibjavaid605@gmail.com>
2023-02-27 15:06:12 +0530

    add bubble sort

---
+++ Sorting/bubbleSort.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+using namespace std;
+
+// Time Complexity --> O(n^2)
+// Auxilary Space --> O(1)
+ 
+void bubbleSort(int arr[], auto n){
+    for(int i = 0; i < n - 1; i++){
+        for(int j = 0; j < n - i - 1; j++){
+            if(arr[j] > arr[j + 1]){
+                swap(arr[j], arr[j + 1]);
+            }
+        }
+    }
+}
+
+int main() {
+    // Function implements the Bubble Sort
+    int arr[] = {2, 10, 8, 7};
+    auto n = sizeof(arr) / sizeof(int);
+    bubbleSort(arr, n);
+    
+    for(int i = 0; i < n; i++){
+        cout << arr[i] << " ";
+    }
+    cout << endl;
+    
+    return 0;
+}
