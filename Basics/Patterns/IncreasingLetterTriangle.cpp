55ad41f8d973ae27122245cabb962908e39eedb3
aaqibjavaid <aaqibjavaid605@gmail.com>
2023-12-17 18:57:34 +0530

    Increasing Letter Triangle

---
+++ Basics/Patterns/IncreasingLetterTriangle.cpp
@@ -0,0 +1,13 @@
+void nLetterTriangle(int n)
+{
+    for (int i = 1; i <= n; i++)
+    {
+        char ch = 'A';
+        for (int j = 1; j <= i; j++)
+        {
+            cout << ch << " ";
+            ch++;
+        }
+        cout << endl;
+    }
+}
