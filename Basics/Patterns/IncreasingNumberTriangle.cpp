61f15c5debb8d4ffc3b3495b1284d6ca37099426
aaqibjavaid <aaqibjavaid605@gmail.com>
2023-12-17 18:53:13 +0530

    Increasing Number Triangle

---
+++ Basics/Patterns/IncreasingNumberTriangle.cpp
@@ -0,0 +1,13 @@
+void nNumberTriangle(int n)
+{
+    int numberToPrint = 1;
+    for (int i = 1; i <= n; i++)
+    {
+        for (int j = 1; j <= i; j++)
+        {
+            cout << numberToPrint << " ";
+            numberToPrint++;
+        }
+        cout << endl;
+    }
+}
