9851d6dc7259812e2dc391f3546ee8b38d1d8a14
aaqibjavaid <aaqibjavaid605@gmail.com>
2024-01-30 17:11:10 +0530

    add: Longest Subarray With Sum K

---
+++ Arrays/LongestSubarrayWithSumK.cpp
@@ -0,0 +1,30 @@
+int longestSubarrayWithSumK(vector<int> a, long long k)
+{
+    int left = 0;
+    int right = 0;
+    int maxLength = 0;
+    long long sum = a[0];
+    int n = a.size();
+
+    while (right < n)
+    {
+        while (left <= right && sum > k)
+        {
+            sum -= a[left];
+            left++;
+        }
+
+        if (sum == k)
+        {
+            maxLength = max(maxLength, right - left + 1);
+        }
+
+        right++;
+        if (right < n)
+        {
+            sum += a[right];
+        }
+    }
+
+    return maxLength;
+}
