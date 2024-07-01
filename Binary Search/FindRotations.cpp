85cb5941f27cbad9b17865780197ef8277cfba76
aaqibjavaid <aaqibjavaid605@gmail.com>
2024-07-01 13:01:52 +0530

    add: Find the number of times the sorted array has been rotated

---
+++ Binary Search/FindRotations.cpp
@@ -0,0 +1,39 @@
+class Solution
+{
+public:
+    int findKRotation(int arr[], int n)
+    {
+        int start = 0;
+        int end = n - 1;
+        int ans = 0;
+        int minElement = INT_MAX;
+
+        while (start <= end)
+        {
+            int mid = start + (end - start) / 2;
+
+            if (arr[start] <= arr[mid])
+            {
+                if (arr[start] < minElement)
+                {
+                    minElement = arr[start];
+                    ans = start;
+                }
+
+                start = mid + 1;
+            }
+            else
+            {
+                if (arr[mid] < minElement)
+                {
+                    minElement = arr[mid];
+                    ans = mid;
+                }
+
+                end = mid - 1;
+            }
+        }
+
+        return ans;
+    }
+};
