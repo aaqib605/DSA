b0294f356de99b09d3420ad89b54b8323bdc39bc
aaqibjavaid <aaqibjavaid605@gmail.com>
2024-07-30 16:48:03 +0530

    feat: convert array into linked list

---
+++ Linked List/ConvertArrToLinkedList.cpp
@@ -0,0 +1,38 @@
+#include <bits/stdc++.h>
+
+class Node
+{
+public:
+    int data;
+    Node *next;
+
+    Node(int data, Node *next)
+    {
+        this->data = data;
+        this->next = next;
+    }
+};
+
+Node *convertArrToLinkedList(int arr[], int size)
+{
+    Node *head = new Node(arr[0], nullptr);
+    Node *mover = head;
+
+    for (int i = 1; i < size; i++)
+    {
+        Node *temp = new Node(arr[i], nullptr);
+        mover->next = temp;
+        mover = temp;
+    }
+
+    return head;
+}
+
+int main()
+{
+    int arr[5] = {10, 15, 20, 25, 30};
+    Node *head = convertArrToLinkedList(arr, 5);
+
+    cout << head->data << endl;
+    return 0;
+}
