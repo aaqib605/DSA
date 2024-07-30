c7ef110c251d17594c77b50f127d9b96ec4e5491
aaqibjavaid <aaqibjavaid605@gmail.com>
2024-07-30 17:28:29 +0530

    feat: find length of a linked list

---
+++ Linked List/LengthOfLinkedList.cpp
@@ -0,0 +1,55 @@
+#include <bits/stdc++.h>
+
+class Node
+{
+public:
+    int data;
+    Node *next;
+
+    Node(int data, Node *next)
+    {
+        this->data = data;
+        this->next = next;
+    }
+};
+
+Node *convertArrToLinkedList(int arr[], int size)
+{
+    Node *head = new Node(arr[0], nullptr);
+    Node *mover = head;
+
+    for (int i = 1; i < size; i++)
+    {
+        Node *temp = new Node(arr[i], nullptr);
+        mover->next = temp;
+        mover = temp;
+    }
+
+    return head;
+}
+
+int findLinkedListLength(Node *head)
+{
+    Node *temp = head;
+    int length = 0;
+
+    while (temp)
+    {
+        length++;
+        temp = temp->next;
+    }
+
+    return length;
+}
+
+int main()
+{
+    int arr[5] = {10, 15, 20, 25, 30};
+    Node *head = convertArrToLinkedList(arr, 5);
+
+    int lengthOfLinkedList = findLinkedListLength(head);
+
+    cout << "length of linked list: " << lengthOfLinkedList << endl;
+
+    return 0;
+}
